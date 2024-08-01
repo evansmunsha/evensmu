cab7e66e31785cbbca4b8ad4033e0c5e3a2eb86f
evansmunsha <evansensteen@gmail.com>
2024-08-01 20:54:46 +0200

    "therewaserror"

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,4 +1,4 @@
-import loginImage from "@/assets/login-image.jpg";
+import loginImage from "@/assets/login-image.png";
 import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
@@ -13,8 +13,15 @@ export default function Page() {
   return (
     <main className="flex h-screen items-center justify-center p-5">
       <div className="flex h-full max-h-[40rem] w-full max-w-[64rem] overflow-hidden rounded-2xl bg-card shadow-2xl">
-        <div className="w-full space-y-10 overflow-y-auto p-10 md:w-1/2">
-          <h1 className="text-center text-3xl font-bold">Login to bugbook</h1>
+        <div className="w-full space-y-10 overflow-y-auto scrollbar-hide p-10 md:w-1/2">
+          <div className="space-y-1 text-center">
+
+            <h1 className="text-center text-3xl font-bold">Login to evans<span className="text-red-600">book</span></h1>
+            <p className="">
+                To  <span className="italic bg-red-600 text-white rounded-xl px-1">chat</span> with
+                your friends.
+              </p>
+          </div>
           <div className="space-y-5">
             <GoogleSignInButton />
             <div className="flex items-center gap-3">
@@ -23,14 +30,14 @@ export default function Page() {
               <div className="h-px flex-1 bg-muted" />
             </div>
             <LoginForm />
-            <Link href="/signup" className="block text-center hover:underline">
+            <Link href="/signup" className="block text-center text-orange-700 hover:underline font-bold">
               Don&apos;t have an account? Sign up
             </Link>
           </div>
         </div>
         <Image
           src={loginImage}
-          alt=""
+          alt="image"
           className="hidden w-1/2 object-cover md:block"
         />
       </div>
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,8 +1,9 @@
-import signupImage from "@/assets/signup-image.jpg";
+import signupImage from "@/assets/signup-image.png";
 import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
 import SignUpForm from "./SignUpForm";
+import GoogleSignInButton from "../login/google/GoogleSignInButton";
 
 export const metadata: Metadata = {
   title: "Sign Up",
@@ -11,25 +12,31 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <main className="flex h-screen items-center justify-center p-5">
-      <div className="flex h-full max-h-[40rem] w-full max-w-[64rem] overflow-hidden rounded-2xl bg-card shadow-2xl">
-        <div className="w-full space-y-10 overflow-y-auto p-10 md:w-1/2">
+      <div className="flex h-full max-h-[40rem] w-full max-w-[64rem] overflow-hidden rounded bg-card shadow-2xl">
+        <div className="w-full space-y-10 overflow-y-auto scrollbar-hide p-6 md:w-1/2">
           <div className="space-y-1 text-center">
-            <h1 className="text-3xl font-bold">Sign up to bugbook</h1>
+            <h1 className="text-3xl font-bold">Sign up to evans<span className="text-red-600">book</span></h1>
             <p className="text-muted-foreground">
-              A place where even <span className="italic">you</span> can find a
+              A place where  <span className="italic bg-red-600 text-white rounded-xl">everyone</span> can find a
               friend.
             </p>
           </div>
-          <div className="space-y-5">
+          <div className="space-y-3">
+            <GoogleSignInButton />
+            <div className="flex items-center gap-3">
+              <div className="h-px flex-1 bg-muted" />
+              <span>OR</span>
+              <div className="h-px flex-1 bg-muted" />
+            </div>
             <SignUpForm />
-            <Link href="/login" className="block text-center hover:underline">
+            <Link href="/login" className="block text-center text-orange-700 hover:underline font-bold">
               Already have an account? Log in
             </Link>
           </div>
         </div>
         <Image
           src={signupImage}
-          alt=""
+          alt="image"
           className="hidden w-1/2 object-cover md:block"
         />
       </div>
